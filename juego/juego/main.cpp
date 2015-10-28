c72d8fc51dea893391f240ed995d3bb84accbd28
jlogzz <juan_lorenzo_@hotmail.com>
2015-10-28 14:22:13 -0600

    Menu

--- juego/juego/main.cpp
+++ juego/juego/main.cpp
@@ -44,8 +44,6 @@ int anguloTetera = 0;
 int opt = 0;
 int tetera = 0;
 
-static int shoulder = 0, elbow = 0, hand = 0, finger = 0;
-
 void myTimer(int opcion) {
 	if (opt) {
 		if (angulo < 370) {
@@ -64,7 +62,7 @@ void myTimer(int opcion) {
 void output(GLfloat x, GLfloat y, char* text, int size, int weight = 1)
 {
 	glPushMatrix();
-	glTranslatef(x, y, 0);
+	glTranslatef(x, y, 0.1f);
 	int fontsize = size;
 	glLineWidth(weight);
 	glScalef(fontsize / 152.38, fontsize / 152.38, fontsize / 152.38);
@@ -78,27 +76,9 @@ void output(GLfloat x, GLfloat y, char* text, int size, int weight = 1)
 void display() {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	double xOffset = 200;
-
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
-	gluOrtho2D(0, ancho, 0, alto);
-
-	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-
 	if (viewDisplay == 0) { //menu
-		glPushMatrix();
-		glTranslatef(400, 300, 0.1);
-		glScalef(800, 600, 0);
-		glColor3ub(139, 69, 19);
-		glutSolidCube(1);
-		glColor3ub(139, 69, 19);
-		glutWireCube(1);
-		glPopMatrix();
-
 		glColor3ub(0, 0, 0);
-		output(100, 500, "MUGRE SUCIEDAD", 80, 8);
+		output(-300, 200, "MUGRE SUCIEDAD", 80, 8);
 
 		if (menuOpcion == 0) {
 			glColor3ub(255, 255, 255);
@@ -106,7 +86,7 @@ void display() {
 		else {
 			glColor3ub(0, 0, 0);
 		}
-		output(220, 340, "NUEVO JUEGO", 50, 4);
+		output(-220, 50, "NUEVO JUEGO", 50, 4);
 
 		if (menuOpcion == 1) {
 			glColor3ub(255, 255, 255);
@@ -114,7 +94,7 @@ void display() {
 		else {
 			glColor3ub(0, 0, 0);
 		}
-		output(220, 270, "INSTRUCIONES", 50, 4);
+		output(-220, -20, "INSTRUCIONES", 50, 4);
 
 		if (menuOpcion == 2) {
 			glColor3ub(255, 255, 255);
@@ -122,7 +102,7 @@ void display() {
 		else {
 			glColor3ub(0, 0, 0);
 		}
-		output(220, 200, "CREDITOS", 50, 4);
+		output(-220, -90, "CREDITOS", 50, 4);
 
 		if (menuOpcion == 3) {
 			glColor3ub(255, 255, 255);
@@ -130,80 +110,42 @@ void display() {
 		else {
 			glColor3ub(0, 0, 0);
 		}
-		output(220, 130, "SALIR", 50, 4);
+		output(-220, -160, "SALIR", 50, 4);
 	}
 	else if (viewDisplay == 1) { //ayuda
+		glColor3ub(0, 0, 0);
+		output(-300, 200, "MUGRE SUCIEDAD", 80, 8);
 
+		output(-360, 100, "Utiliza las teclas: ARRIBA, ABAJO, IZQUIERDA, DERECHA", 30, 2);
+		output(-360, 60, "para mover la cubeta. Debes de atrapar las gotas de", 30, 2);
+		output(-360, 20, "agua para poder lavarte los dientes.", 30, 2);
+
+		output(-400, -250, "Presiona ESC para regresar", 40, 4);
 	}
 	else if (viewDisplay == 2) { //juego
-		glLineWidth(3);
-
-		glPushMatrix();
-		glTranslatef(-1.0, 0.0, 0.0);
-		glRotatef((GLfloat)shoulder, 0.0, 0.0, 1.0);
-		glTranslatef(1.0, 0.0, 0.0);
-
-		glPushMatrix();
-		glScalef(2.0, 0.4, 1.0);
-		glColor3f(2, 0.4, 1);
-		glutSolidCube(1.0);
-		glColor3f(1, 1, 1);
-		glutWireCube(1.0);
-		glPopMatrix();
-
-		glTranslatef(1.0, 0.0, 0.0);
-		glRotatef((GLfloat)elbow, 0.0, 0.0, 1.0);
-		glTranslatef(1.0, 0.0, 0.0);
-
-		glPushMatrix();
-		glScalef(2.0, 0.4, 1.0);
-		glColor3f(0, 0, 1);
-		glutWireCube(1.0);
-
-		glPopMatrix();
-
-
-		glTranslatef(1, 0.0, 0.0);
-		glRotatef((GLfloat)hand, 0.0, 0.0, 1.0);
-		glTranslatef(0.25, 0.0, 0.0);
-
-		glPushMatrix();
-		glScalef(0.5, 0.5, 0.5);
-		glColor3f(1, 1, 1);
-		glutSolidCube(1.0);
-		glColor3f(1, 0, 1);
-		glutWireCube(1.0);
-
-		glPopMatrix();
-
-		glTranslatef(0.25, 0.0, 0.0);
-		glRotatef((GLfloat)finger, 1, 0.0, 0);
-		glTranslatef(0.25, 0.0, 0.0);
-
-		glPushMatrix();
-		glScalef(0.25, 0.25, 1);
-		glColor3f(1, 0, 0);
-		glutSolidCube(1.0);
-		glColor3f(1, 1, 1);
-		glutWireCube(1.0);
-
-		glPopMatrix();
-
-		glPopMatrix();
-
-		glPopMatrix();
+		
 	}
 	else if (viewDisplay == 3) { //pausa
+		glColor3ub(0, 0, 0);
+		output(-300, 200, "MUGRE SUCIEDAD", 80, 8);
 
+		output(-400, -250, "Presiona ESC para regresar", 40, 4);
 	}
 	else if (viewDisplay == 4) { //game over
 
 	}
 	else if (viewDisplay == 5) { //creditos
+		glColor3ub(0, 0, 0);
+		output(-300, 200, "MUGRE SUCIEDAD", 80, 8);
+
+		output(-360, 100, "Juan Lorenzo Gonzalez - A01190381", 30, 2);
+		output(-360, 60, "Roland Alexander Baumann - A01190690", 30, 2);
+
+		output(-400, -250, "Presiona ESC para regresar", 40, 4);
 
 	}
 	else {
-
+		
 	}
 
 	glutSwapBuffers();
@@ -212,8 +154,9 @@ void display() {
 
 void init(void)
 {
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glShadeModel(GL_SMOOTH);
+	glClearColor(139/255.0, 69 / 255.0, 19 / 255.0, 0.0);
+	glShadeModel(GL_FLAT);
+	glEnable(GL_DEPTH_TEST);
 }
 
 void onMenu(int op) {
@@ -254,39 +197,13 @@ void myKeyboard(unsigned char theKey, int x, int y)
 	//cout << theKey << endl;
 	switch (theKey)
 	{
-	case 's':   /*  s key rotates at shoulder  */
-		shoulder = (shoulder + 5) % 360;
-		glutPostRedisplay();
-		break;
-	case 'S':
-		shoulder = (shoulder - 5) % 360;
-		glutPostRedisplay();
-		break;
-	case 'e':  /*  e key rotates at elbow  */
-		elbow = (elbow + 5) % 360;
-		glutPostRedisplay();
-		break;
-	case 'E':
-		elbow = (elbow - 5) % 360;
-		glutPostRedisplay();
-		break;
-
-	case 'd':  /*  e key rotates at elbow  */
-		hand = (hand + 5) % 360;
-		glutPostRedisplay();
-		break;
-	case 'D':
-		hand = (hand - 5) % 360;
-		glutPostRedisplay();
-
-
-	case 'w':  /*  e key rotates at elbow  */
-		finger = (finger + 5) % 360;
-		glutPostRedisplay();
-		break;
-	case 'W':
-		finger = (finger - 5) % 360;
-		glutPostRedisplay();
+	case 27:
+		if (menuOpcion == 1 && viewDisplay == 1) {
+			viewDisplay = 0;
+		}
+		else if (menuOpcion == 2 && viewDisplay == 5) {
+			viewDisplay = 0;
+		}
 		break;
 	case 13:
 		if (menuOpcion == 3 && viewDisplay == 0) {
@@ -294,7 +211,13 @@ void myKeyboard(unsigned char theKey, int x, int y)
 			//terminate the program
 		}
 		else if (menuOpcion == 0 && viewDisplay == 0) {
-			viewDisplay = 2;
+			viewDisplay = 2;//juego
+		}
+		else if (menuOpcion == 1 && viewDisplay == 0) {
+			viewDisplay = 1;//ayuda
+		}
+		else if (menuOpcion == 2 && viewDisplay == 0) {
+			viewDisplay = 5;//creditos
 		}
 	default:
 		break;		      // do nothing
@@ -307,13 +230,13 @@ void mySpecialKeyboard(int theKey, int x, int y)
 	switch (theKey)
 	{
 	case 101:
-		if (menuOpcion > 0) {
+		if (menuOpcion > 0 && viewDisplay == 0) {
 			menuOpcion--;
 		}
 		glutPostRedisplay();
 		break;
 	case 103:
-		if (menuOpcion < 3) {
+		if (menuOpcion < 3 && viewDisplay == 0) {
 			menuOpcion++;
 		}
 		glutPostRedisplay();
@@ -324,14 +247,16 @@ void mySpecialKeyboard(int theKey, int x, int y)
 }
 
 void reshape(int w, int h) {
-	glViewport(0, 0, (GLsizei)w, (GLsizei)h);
+	glViewport(0, 0, w, h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluPerspective(65.0, (GLfloat)w / (GLfloat)h, 1.0, 20.0);
-
+	if (w <= h)
+		glOrtho(-400, 400, -300*(GLfloat)h / (GLfloat)w,
+			300*(GLfloat)h / (GLfloat)w, -10.0, 0.0);
+	else
+		glOrtho(-400*(GLfloat)w / (GLfloat)h,
+			400*(GLfloat)w / (GLfloat)h, -300, 300, -10.0, 0.0);
 	glMatrixMode(GL_MODELVIEW);
-	glLoadIdentity();
-	gluLookAt(0, 0, 5, 0, 0, 0, 0, 1, 0);
 }
 
 
@@ -343,7 +268,7 @@ int main(int argc, char *argv[])
 	glutInit(&argc, argv);
 	glutInitWindowSize(ancho, alto);
 	glutInitWindowPosition(10, 10);
-	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	int mainWindow = glutCreateWindow("Mugre Suciedad");
 	init();
 	glutDisplayFunc(display);
